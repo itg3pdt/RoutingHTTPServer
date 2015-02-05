9f96f33b446214625b4d9eb8aadebdd68823bef8
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-02-05 08:35:32 -0500

    fixed Test case not compiling for OS X

--- Framework/RoutingHTTPServer/RoutingHTTPServerTests/RoutingHTTPServerTests.m
+++ Framework/RoutingHTTPServer/RoutingHTTPServerTests/RoutingHTTPServerTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Alloysoft. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 
 @interface RoutingHTTPServerTests : XCTestCase
